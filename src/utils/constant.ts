7b135b6c1cd151207c52f251e62bef36fbe64355
Otchi <ouchihaamine@gmail.com>
2024-12-15 22:32:30 +0100

    fix regexp to target pattern

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,7 +2,7 @@
 export const UTF8 = 'utf-8'
 //
 export const LINK_STYLESHEET_REGEXP =
-  /<link.*rel=['|"]stylesheet['|"][^>]+href="([^"]+\.css)"[^>]*>/g
+  /<link[^>]*rel=['|"]stylesheet['|"][^>]+href="([^"]+\.css)"[^>]*>/g
 //
 export const SCRIPT_REGEXP = /<script[^>]+src="([^"]+)"[^>]*>/g
 //
@@ -10,7 +10,7 @@ export const STYLE_REGEXP = /<style[^>]*>([^<]+)<\/style>/g
 //
 export const IMG_REGEXP = /<img[^>]+src="([^"]+)"[^>]*>/g
 //
-export const STYLE_URL_REGEXP = /url.*\((.*)\)/g
+export const STYLE_URL_REGEXP = /url[^)]*\(([^)]*)\)/gm
 
 export const BASE64_PREFIX_STR = 'data:image/png;base64,'
 //
